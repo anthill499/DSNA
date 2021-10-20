2bb7f99625765f71364d83b7e97933bc0bf04a62
Anthony Huang <anthill499@gmail.com>
2021-10-20 16:21:31 -0400

    questions

---
+++ leetcode/sqrt.js
@@ -0,0 +1,20 @@
+// easy
+//Given a non-negative integer x, compute and return the square root of x.
+//Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+//Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+const mySqrt = (x) => {
+  let left = 0;
+  let right = x;
+  let mid;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (mid * mid === x) {
+      return mid;
+    } else if (mid * mid > x) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+};
