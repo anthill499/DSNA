ffbda1d42af7936cb9a93ce18a2fb231d5b30a1f
Anthony Huang <anthill499@gmail.com>
2021-12-11 15:25:40 -0500

    lol

--- leetcode/linkedLists/palindromeLinkedList.js
+++ leetcode/linkedLists/palindromeLinkedList.js
@@ -1,10 +1,12 @@
 var isPalindrome = function (head) {
-  let [values, curr, pA] = [[], head, 0];
+  let values = [];
+  let curr = head;
+  let pA = 0;
+  let pB = values.length - 1;
   while (curr) {
     values.push(curr.val);
     curr = curr.next;
   }
-  let pB = values.length - 1;
   while (pB > pA) {
     if (values[pA] !== values[pB]) return false;
     pA++;
