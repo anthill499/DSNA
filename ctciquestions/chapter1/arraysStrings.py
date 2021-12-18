beecfc11a5863b819fbf6e48790366c04eea68ed
Anthony Huang <anthill499@gmail.com>
2021-12-18 02:14:55 -0500

    arrayStrings

--- ctciquestions/chapter1/arraysStrings.py
+++ ctciquestions/chapter1/arraysStrings.py
@@ -3,7 +3,25 @@ def printStrings(num = 1):
     if num % 2 == 0:
         print("Number is even")
     else:
-        print("Number is odd")
-        
-printStrings(2)
-printStrings(1)
+        print("Number is odd")        
+# printStrings(2)
+# printStrings(1)
+
+def printExponent():
+    print(2 ** 3)
+def printMod():
+    print(4 % 2)
+def printDivision():
+    print(4 / 2)
+def printInteger():
+    print(5 // 2)  
+    
+# printExponent()
+# printMod()
+# printDivision()
+# printInteger()
+
+def printBang(): 
+    print(not 3)
+    
+printBang()
