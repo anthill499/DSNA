a8a720f93beb94a46704439cd1ebf8bd0c8f17f5
Anthony Huang <anthill499@gmail.com>
2021-12-18 19:34:42 -0500

    goal parser

---
+++ leetcode/strings/goalParser.py
@@ -0,0 +1,17 @@
+# You own a Goal Parser that can interpret a string command. 
+# The command consists of an alphabet of "G", "()" and/or "(al)" in some order. 
+# The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" 
+# as the string "al". The interpreted strings are then concatenated in the original order.
+# Given the string command, return the Goal Parser's interpretation of command.
+
+class Solution:
+    def interpret(self, command: str) -> str:
+        result = ""
+        stack = ""
+        terms = {'G': 'G', '()': 'o', '(al)': 'al'} 
+        for letter in command:
+            stack += letter
+            if stack in terms:
+                result += terms[stack]
+                stack = ""
+        return result
