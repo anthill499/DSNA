a56ea232bacd12fafea7a91942fdf336c3e767a0
Anthony Huang <anthill499@gmail.com>
2021-12-19 22:31:42 -0500

    o(n)

--- leetcode/dynamicProgramming/minCostClimbingStairs.js
+++ leetcode/dynamicProgramming/minCostClimbingStairs.js
@@ -13,3 +13,29 @@ var minCostClimbingStairs = function (cost) {
   };
   return Math.min(findSum(cost, {}), findSum(cost.slice(1), {}));
 };
+
+// Key Takeaway
+// Time Complexity: O(2^n)
+
+// More optimized solution
+// Time Complexity: O(2^n)
+// Memoization
+var minCostClimbingStairs = function (cost) {
+  if (cost.length < 3) return Math.min(...cost);
+  const store = {};
+  const findSum = (array, hash = store) => {
+    // base case
+    if (array.length === 0) return 0;
+    if (array.length === 1) return array[0];
+    // inductive step
+    if (hash[array]) return hash[array];
+    const sum =
+      array[0] +
+      Math.min(findSum(array.slice(1), hash), findSum(array.slice(2), hash));
+    hash[array] = sum;
+    return sum;
+  };
+
+  const wholeSum = findSum(cost, store);
+  return Math.min(wholeSum, store[cost.slice(1)]);
+};
