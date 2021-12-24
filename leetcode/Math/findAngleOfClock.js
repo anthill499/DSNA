f945d6ec2a931111b8fb4d6d8ca56f3cf185fd37
Anthony Huang <anthill499@gmail.com>
2021-12-23 23:19:28 -0500

    lol

--- leetcode/Math/findAngleOfClock.js
+++ leetcode/Math/findAngleOfClock.js
@@ -3,9 +3,14 @@
 // Write an algorithm determining the angle between the two hands on the clock
 
 const findHour = (h, m) => {
-  const minAngle = m * 6;
+  const minAngle = m * 6; // we want to convert the minute hand to degrees
   const hrAngle = h * 30 + m * 0.5;
+  // h * 30 because we want to covert the hour hand to degrees
+  // m * 0.5 because 30 deg per hour hand
+  // hrAngle = (30 x h) + (30 x m/60) second part is a percentage through the hour
+
   const diff = Math.abs(hrAngle - minAngle);
+  // Abs value to consider the hours that are past 6 like 6:30
   return Math.min(360 - diff, diff);
 };
 
--- leetcode/Math/makeCoinFlipFair.js
+++ leetcode/Math/makeCoinFlipFair.js
@@ -3,10 +3,17 @@ const rand2 = () => {
 };
 
 // Supposed we have a function that a function rand2
-// How do we make rand3 give fair chances to three people?
+// How do we make a function rand3 give fair chances to three people?
 
+// 0 0 25%
+// 1 0 50%
+// 0 1
+// 1 1 25%
+
+// 1 0 0 = fair
+// 0 0 1 = fair
 const rand3 = () => {
-  const store = [rand2(), rand2(), rand()];
+  const store = [rand2(), rand2(), rand2()];
   const sum = store.reduce((acc, next) => {
     return acc + next;
   }, 0);
@@ -16,3 +23,8 @@ const rand3 = () => {
     return rand3();
   }
 };
+
+const newArr = [...new Array(1000)].map((_) => rand3());
+console.log(newArr.filter((ele) => ele === 0).length);
+console.log(newArr.filter((ele) => ele === 1).length);
+console.log(newArr.filter((ele) => ele === 2).length);
