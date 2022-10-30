54510cbc6413a558ab10306d0f6f44c5588de350
anthill499 <anthill499@gmail.com>
2022-10-30 18:37:02 -0400

    stacks methods

--- DataStructures/stacksAndQueues/Stack.js
+++ DataStructures/stacksAndQueues/Stack.js
@@ -2,6 +2,34 @@ class Stack {
   constructor(items = []) {
     this.items = items;
   }
+
+  push(value) {
+    this.items.push(value);
+  }
+
+  peek() {
+    const topMostValue = this.items[this.items.length - 1];
+    return topMostValue === undefined ? null : topMostValue;
+  }
+
+  pop() {
+    const removedValue = this.items.pop();
+    return removedValue !== undefined ? removedValue : null;
+  }
+
+  search(value) {
+    const l = this.items.length;
+    if (l === 0) return -1;
+    for (let index = 0; index < l; index++) {
+      if (this.items[index] === value) {
+        return l - index;
+      }
+    }
+    return -1;
+  }
 }
 
-const stack = new Stack();
+// const stack = new Stack();
+const stack = new Stack([]);
+
+console.log(stack.search("d1"));
